320c559ed721cbb194c541d15709d2f93ef6cfbe
Nguyen Hong Dien <HongDien16102004@gmail.com>
2026-03-28 19:59:34 +0700

    thêm endpoint lấy report list

--- src/BookLAB.API/Controllers/ReportsController.cs
+++ src/BookLAB.API/Controllers/ReportsController.cs
@@ -1,8 +1,13 @@
 ﻿using BookLAB.Api.Controllers;
+using BookLAB.Application.Common.Models;
 using BookLAB.Application.Features.Bookings.Queries.ViewBookingHistory;
 using BookLAB.Application.Features.IncidentReports.Queries.GetReportedReport;
+using BookLAB.Application.Features.IncidentReports.Queries.GetReports;
+using BookLAB.Domain.Entities;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Globalization;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -34,6 +39,44 @@ namespace BookLAB.API.Controllers
                 };
 
                 var result = await _mediator.Send(command);
+
+                return Ok(new
+                {
+                    success = true,
+                    data = result
+                });
+            }
+            catch (Exception ex)
+            {
+                return Problem("Something is wrong");
+            }
+
+        }
+
+        [HttpGet("listreports")]
+        public async Task<IActionResult> GetUnresolvedReports([FromQuery] ReportRequestDto dto)
+        {
+            try
+            {
+                Guid.TryParse(HttpContext.User.FindFirst("Id")?.Value, out var userId);
+
+                GetReportsQuery query = new GetReportsQuery
+                {
+                    Q = dto.Q,
+                    BuildingId = dto.BuildingId,
+                    RoomId = dto.RoomId,
+                    ReportType = dto.ReportType,
+                    IsResolved = dto.IsResolved,
+                    FromDate = dto.FromDate,
+                    ToDate = dto.ToDate,
+                    Page = dto.Page,
+                    Limit = dto.Limit,
+                    SortBy = dto.SortBy,
+                    IsDescending = dto.IsDescending,
+                    UserId = userId
+                };
+
+                var result = await _mediator.Send(query);
 
                 return Ok(new
                 {
