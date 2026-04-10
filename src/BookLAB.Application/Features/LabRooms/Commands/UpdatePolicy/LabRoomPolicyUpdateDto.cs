b266bf782e99139b6a4648b014eea3a7b7257662
Trung Phan <trunggallagher2312@gmail.com>
2026-04-10 20:19:49 +0700

    Update Policy

---
+++ src/BookLAB.Application/Features/LabRooms/Commands/UpdatePolicy/LabRoomPolicyUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace BookLAB.Application.Features.LabRooms.Commands.UpdatePolicy
+{
+    public class LabRoomPolicyUpdateDto
+    {
+        public int LabRoomId { get; set; }
+        public string PolicyKey { get; set; } = string.Empty;
+        public string PolicyValue { get; set; } = string.Empty;
+        public bool IsActive { get; set; }
+    }
+}
